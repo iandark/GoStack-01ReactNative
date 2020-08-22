a385b91b4bca75a32ab5989df097c67ebebbc7bc
iandark <derkian@iands.com.br>
2020-08-22 19:46:32 -0300

    feat: Inclusão de axios para buscar lista de projetos

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 ### Conteúdo 03 do GoStack da RocketSeat, este repositório aborda sobre:
 
-- Estilização de componentes
+*   Estilização de componentes
+*   Utilização do axios para se comunicar ao backend (baseURL)
+    *   **iOS com emulador:** localhost
+    *   **iOS com físico:** ip da máquina
+    *   **Android com emulador:** localhost (adb reverse tcp:3333 tcp:3333)
+    *   **Android com emulador:** 10.0.2.2 (Android studio)
+    *   **Android com emulador:** 10.0.3.2 (Genymotion)
+    *   **Android com físico:** ip da máquina
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, StatusBar } from "react-native";
+import api from "./services/api";
 
 export default function App() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get("projects").then((response) => {
+            console.log(response.data);
+            setProjects(response.data);
+        });
+    }, []);
+
     return (
         <>
             <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
